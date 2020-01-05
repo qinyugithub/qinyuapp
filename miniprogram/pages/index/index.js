531ee59ade2964bbc9b4bae6ce00208d6fa0947f
qinyu <1326609017@qq.com>
2020-01-05 21:48:33 +0800

    首页基本样式实现

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,18 +1,49 @@
-// pages/index/index.js
+const db = wx.cloud.database();
+const swiperImages = db.collection('swiperImages');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    swiperImages:[],
+    inputShowed: false,
+    inputVal: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.initSwiper();
+    this.setData({
+      search: this.search.bind(this)
+  })
+  },
 
+  search: function (value) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}])
+        }, 200)
+    })
+},
+selectResult: function (e) {
+    console.log('select result', e.detail)
+},
+
+
+
+
+  /**
+   * 初始化轮播图
+   */
+  initSwiper:function(){
+    swiperImages.get().then(res =>{
+      this.setData({
+        swiperImages:res.data
+      })
+    })
   },
 
   /**
